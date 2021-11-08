d517322cf43b9c392a4fc86871a96db3f632d38f
hectoraldairah <hectoraldairah@protonmail.com>
2021-11-07 22:23:56 -0600

    add post and blog page

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,24 @@
 module.exports = config => {
 
+    // Filters
+  const dateFilter = require('./src/filters/date-filters');
+  const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/images/');
+
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
+
+
+   // Returns a collection of blog posts in reverse date order
+   config.addCollection('blog', (collection) => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+  });
+
+  // Add filters
+  config.addFilter('postDate', dateFilter);
+  config.addFilter('w3Date', w3DateFilter);
 
     return {
       dir: {
---
+++ src/blog.md
@@ -0,0 +1,11 @@
+---
+title: "Blog"
+layout: "layouts/feed.njk"
+pagination:
+  data: collections.blog
+  size: 5
+paginationPrevText: "Newer posts"
+paginationNextText: "Older posts"
+paginationAnchor: "#post-list"
+permalink: "blog{% if pagination.pageNumber > 0 %}/page/{{ pagination.pageNumber }}{% endif %}/index.html"
+---
