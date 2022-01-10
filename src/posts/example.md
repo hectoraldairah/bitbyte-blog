5526a39db97815fa5ee86f81c546e3530028c9f1
hectoraldairah <hectoraldairah@protonmail.com>
2022-01-09 22:23:30 -0600

    Fix: Change First post .md name

---
+++ src/posts/example.md
@@ -0,0 +1,16 @@
+---
+title: "Example Post"
+date: 2022-12-14
+tags: ["Personal"]
+featured: true
+description: "Example."
+---
+
+# My title is crazy
+some test
+
+## My title is crazy
+Some test
+
+### Title 3
+Some test
--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -1,32 +1,42 @@
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const slugify = require("slugify");
 
-const anchorSlugify = (s) =>
-  encodeURIComponent(
-    "h-" +
-      String(s)
-        .trim()
-        .toLowerCase()
-        .replace(/[.,\/#!$%\^&\*;:{}=_`~()]/g, "")
-        .replace(/\s+/g, "-")
-  );
-
-const markdown = markdownIt({
-  html: true,
-  breaks: true,
-  typographer: true,
-  linkify: true,
-}).use(markdownItAnchor, {
-  permalink: markdownItAnchor.permalink.ariaHidden({
-    placement: "before",
-    class: "direct-link",
-    symbol: "#",
-    level: [1, 2, 3, 4],
-  }),
-  permalinkBefore: true,
-  permalinkAttrs: () => ({ "aria-hidden": true }),
-  level: 2,
-  slugify: anchorSlugify,
+const linkBeforeHeader = markdownItAnchor.permalink.linkAfterHeader({
+  class: "anchor",
+  symbol: "<span hidden>#</span>",
+  style: "aria-labelledby",
 });
+
+const markdownItOptions = {
+  level: [1, 2, 3],
+  slugify: (str) => slugify(str, { lower: true, strict: true, remove: /["]/g }),
+  tabIndex: false,
+  permalink(slug, opts, state, idx) {
+    state.tokens.splice(
+      idx,
+      0,
+      Object.assign(new state.Token("div_open", "div", 1), {
+        // Add class "header-wrapper [h1 or h2 or h3]"
+        attrs: [["class", `heading-wrapper ${state.tokens[idx].tag}`]],
+        block: true,
+      })
+    );
+    state.tokens.splice(
+      idx + 4,
+      0,
+      Object.assign(new state.Token("div_close", "div", -1), {
+        block: true,
+      })
+    );
+
+    linkBeforeHeader(slug, opts, state, idx + 1);
+  },
+};
+
+/* Markdown Overrides */
+let markdown = markdownIt({
+  html: true,
+}).use(markdownItAnchor, markdownItOptions);
 
 module.exports = markdown;
