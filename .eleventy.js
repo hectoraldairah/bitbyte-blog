94483e54c1bd5ef479e52d749c02b675b2a9283a
hectoraldairah <hectoraldairah@protonmail.com>
2022-06-10 15:46:52 -0500

    pixel art section

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,16 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const pluginImage = require("@11ty/eleventy-img");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const pluginRSS = require("@11ty/eleventy-plugin-rss");
+const pluginImage = require('@11ty/eleventy-img');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const pluginRSS = require('@11ty/eleventy-plugin-rss');
 
-const filters = require("./src/utils/filters");
-const markdown = require("./src/utils/markdown");
+const filters = require('./src/utils/filters');
+const markdown = require('./src/utils/markdown');
 
-const CONTENT_GLOBS = { post: "./src/posts/**/*.md", pixelart: "./src/pixelart/**/*.md" };
+const CONTENT_GLOBS = {
+  post: './src/posts/**/*.md',
+  pixelart: './src/pixelart/**/*.md',
+};
 
 module.exports = (config) => {
   // Plugins
@@ -15,11 +18,11 @@ module.exports = (config) => {
   config.addPlugin(pluginRSS);
 
   // Set directories to pass through to the dist folder
-  config.addPassthroughCopy("./src/images/");
-  config.addPassthroughCopy("./src/assets/");
-  config.addPassthroughCopy("./src/icons/");
-  config.addPassthroughCopy("./src/fonts/");
-  config.addPassthroughCopy("./src/pixelart/");
+  config.addPassthroughCopy('./src/images/');
+  config.addPassthroughCopy('./src/assets/');
+  config.addPassthroughCopy('./src/icons/');
+  config.addPassthroughCopy('./src/fonts/');
+  config.addPassthroughCopy('./src/pixelart/');
 
   // Add filters
   Object.keys(filters).forEach((filterName) => {
@@ -27,36 +30,47 @@ module.exports = (config) => {
   });
 
   // Markdown Passing
-  config.setLibrary("md", markdown);
+  config.setLibrary('md', markdown);
 
   // Returns a collection of blog posts in reverse date order
-  config.addCollection("blog", (collection) => {
+  config.addCollection('blog', (collection) => {
     return [...collection.getFilteredByGlob(CONTENT_GLOBS.post)].reverse();
   });
 
-  config.addCollection("featured", (collection) => {
+  config.addCollection('featured', (collection) => {
     return collection
       .getFilteredByGlob(CONTENT_GLOBS.post)
       .filter((item) => item.data.featured)
       .sort((a, b) => b.date - a.date);
   });
 
-  config.addCollection("pixelart", (collection) => {
-    return [...collection.getFilteredByGlob(CONTENT_GLOBS.pixelart)].reverse()
+  config.addCollection('pixelart', (collection) => {
+    return [...collection.getFilteredByGlob(CONTENT_GLOBS.pixelart)].reverse();
   });
+
+  config.addCollection('publish', (collection) => {
+    const pixelart = [
+      ...collection.getFilteredByGlob(CONTENT_GLOBS.pixelart),
+    ].reverse();
+    const blog = [
+      ...collection.getFilteredByGlob(CONTENT_GLOBS.post),
+    ].reverse();
+    return [...pixelart, ...blog].reverse();
+  });
+
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
 
-  config.on("afterBuild", () => {
-    const socialPreviewImagesDir = "dist/assets/images/social-preview-images/";
+  config.on('afterBuild', () => {
+    const socialPreviewImagesDir = 'dist/assets/images/social-preview-images/';
     fs.readdir(socialPreviewImagesDir, (err, files) => {
       if (files.length > 0) {
         files.forEach((filename) => {
-          if (filename.endsWith(".svg")) {
+          if (filename.endsWith('.svg')) {
             let imageUrl = socialPreviewImagesDir + filename;
             pluginImage(imageUrl, {
-              formats: ["jpeg"],
-              outputDir: "./" + socialPreviewImagesDir,
+              formats: ['jpeg'],
+              outputDir: './' + socialPreviewImagesDir,
               filenameFormat: function (id, src, width, format, options) {
                 let outputFilename = filename.substring(0, filename.length - 4);
 
@@ -71,8 +85,8 @@ module.exports = (config) => {
 
   return {
     dir: {
-      input: "src",
-      output: "dist",
+      input: 'src',
+      output: 'dist',
     },
   };
 };
