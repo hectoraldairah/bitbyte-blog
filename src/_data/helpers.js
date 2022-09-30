e94ba977adde8a5de763b90c89c9b3427194f1d2
hectoraldairah <hectoraldairah@protonmail.com>
2022-09-29 20:12:18 -0500

    adding quote

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,3 +1,14 @@
+const quotes = [
+  "I'm tired of JavaScript",
+  'Write simple CSS is hard',
+  'Elixir should be more mainstream',
+  'Nobody wants a ton of JS in their site',
+  'Pixel-art is one of the best art styles',
+  'JSX is ok',
+  'Vue.js should be the number one',
+  "I don't like Next.js",
+];
+
 module.exports = {
   getLinkActiveState(itemUrl, pageUrl) {
     let response = '';
@@ -5,12 +16,14 @@ module.exports = {
     if (itemUrl === pageUrl) {
       response = ' aria-current="page" data-state="active"';
     }
-
     return response;
   },
-
   generateDate: function () {
     const dateObj = new Date();
     return dateObj;
+  },
+  getQuoteOfTheDay: function () {
+    const randomQuote = Math.floor(Math.random() * (quotes.length - 1) + 1);
+    return quotes[randomQuote];
   },
 };
