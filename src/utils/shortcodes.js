ec236cb7467ac15da370e2997b3494d8b8f6984c
hectoraldairah <hectoraldairah@protonmail.com>
2023-06-20 19:32:53 -0600

    fix pixelart images and add new colorscheme

--- src/utils/shortcodes.js
+++ src/utils/shortcodes.js
@@ -1,4 +1,4 @@
-const Image = require('@11ty/eleventy-img');
+const Image = require("@11ty/eleventy-img");
 
 module.exports = {
   image: async function (
@@ -6,25 +6,55 @@ module.exports = {
     alt,
     className = undefined,
     widths = [400, 800, 1280],
-    sizes = '100vw'
+    sizes = "100vw",
   ) {
     if (alt === undefined) {
-      throw new Error('Missing atl prop for image', src);
+      throw new Error("Missing atl prop for image", src);
     }
 
     const options = {
       widths,
-      formats: ['webp', 'jpeg'],
-      urlPath: '/assets/images',
-      outputDir: 'dist/assets/images/',
+      formats: ["webp", "jpeg"],
+      urlPath: "/assets/images",
+      outputDir: "dist/assets/images/",
     };
 
     const imageAttributes = {
       alt,
       sizes,
       class: className,
-      loading: 'lazy',
-      decoding: 'async',
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    const metadata = await Image(src, options);
+
+    return Image.generateHTML(metadata, imageAttributes);
+  },
+  pixelArtImage: async function (
+    src,
+    alt,
+    className = undefined,
+    widths = [400, 800, 1280],
+    sizes = "100vw",
+  ) {
+    if (alt === undefined) {
+      throw new Error("Missing atl prop for image", src);
+    }
+
+    const options = {
+      widths,
+      formats: ["webp", "jpeg"],
+      urlPath: "/pixelart/**/",
+      outputDir: "dist/pixelart/**/",
+    };
+
+    const imageAttributes = {
+      alt,
+      sizes,
+      class: className,
+      loading: "lazy",
+      decoding: "async",
     };
 
     const metadata = await Image(src, options);
