0bb6465d08b287dc1866777106c69f7118121e92
hectoraldairah <hectoraldairah@protonmail.com>
2025-08-19 17:57:53 -0600

    Inject for all html and .js.html files

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -49,7 +49,7 @@ export default function sass() {
 }
 
 export function injectStagitCSS() {
-  return src('dist/stagit/**/*.html')
+  return src(['dist/stagit/**/*.html', 'dist/stagit/**/*.js.html'])
     .pipe(replace('</head>', '<link rel="stylesheet" type="text/css" href="https://bitbyte.blog/scss/stagit/styles.css" />\n</head>'))
     .pipe(replace('src="logo.png"', 'src="https://bitbyte.blog/assets/images/ball.gif"'))
     .pipe(dest('dist/stagit/'));
--- gulp-tasks/stagit.js
+++ gulp-tasks/stagit.js
@@ -2,7 +2,7 @@ import { src, dest } from 'gulp';
 import replace from 'gulp-replace';
 
 export default function injectStagitCSS() {
-  return src('dist/stagit/**/*.html')
+  return src(['dist/stagit/**/*.html', 'dist/stagit/**/*.js.html'])
     .pipe(replace('</head>', '<link rel="stylesheet" type="text/css" href="https://bitbyte.blog/scss/stagit/styles.css" />\n</head>'))
     .pipe(replace('src="logo.png"', 'src="https://bitbyte.blog/assets/images/ball.gif"'))
     .pipe(dest('dist/stagit/'));
