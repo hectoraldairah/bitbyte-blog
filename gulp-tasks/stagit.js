f766219caa1de1cc85f4f5d23263d422fdb6070f
hectoraldairah <hectoraldairah@protonmail.com>
2025-08-19 18:13:45 -0600

    no image tag for stagit

--- gulp-tasks/stagit.js
+++ gulp-tasks/stagit.js
@@ -4,6 +4,6 @@ import replace from 'gulp-replace';
 export default function injectStagitCSS() {
   return src(['dist/stagit/**/*.html', 'dist/stagit/**/*.js.html'])
     .pipe(replace('</head>', '<link rel="stylesheet" type="text/css" href="https://bitbyte.blog/scss/stagit/styles.css" />\n</head>'))
-    .pipe(replace(/src="\.?\.?\/?(logo\.png)" alt="website logo" width="32" height="32"/g, 'src="https://bitbyte.blog/assets/images/ball.gif" alt="" width="150" height="150"'))
+    .pipe(replace(/<img[^>]*logo\.png[^>]*>/g, ''))
     .pipe(dest('dist/stagit/'));
 }
